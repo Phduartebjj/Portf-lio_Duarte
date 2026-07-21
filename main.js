54ef2e6ea302c6944ce9d306243db0740aefad19
maoel <pedrohduarte007@gmail.com>
2026-07-20 23:31:32 -0300

    feat(contact): Add message redirect WhatsApp on keypress

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 import "./src/js/ui.js"
 import "./src/js/projects.js"
 import { initPortfolio } from "./src/js/controller.js"
+import "./src/js/contact.js"
 
 initPortfolio()
--- src/js/controller.js
+++ src/js/controller.js
@@ -1,17 +1,23 @@
 import { showProjects, updateProjectDetails } from "./ui.js";
 import { getProjects, portfolioProjects } from "./projects.js";
+import { contactMessage } from "./contact.js";
 
 const projectsThree = Array.from(document.querySelectorAll(".project"));
+const messageBox = document.getElementById("contact-message");
+
+messageBox.addEventListener("keydown", (e) => {
+  contactMessage(e, messageBox);
+});
+
+projectsThree.forEach((branch) => {
+  branch.addEventListener("click", (e) => {
+    let projectFind = getProjects().find((p) => p.name === branch.textContent);
+    updateProjectDetails(projectFind);
+  });
+});
 
 export function initPortfolio() {
   portfolioProjects();
-  showProjects(projectsThree, getProjects());
-  projectsThree.forEach((branch) => {
-    branch.addEventListener("click", (e) => {
-      let projectFind = getProjects().find((p) => p.name === branch.textContent);
-        console.log(projectFind)
-      updateProjectDetails(projectFind)
 
-    });
-  });
+  showProjects(projectsThree, getProjects());
 }
