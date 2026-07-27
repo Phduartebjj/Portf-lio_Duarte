a28a32d112a49168c3b73ea2152511eac78b376f
Duarte <pedrohduarte007@gmail.com>
2026-07-27 11:02:04 -0300

    feat: add WhatsApp share button
    Also improves responsive layout for mobile screens.

--- src/js/contact.js
+++ src/js/contact.js
@@ -1,24 +1,15 @@
+export function contactMessage() {
+  const messageBox = document.getElementById("contact-message");
 
-export function contactMessage(e, input) {
-  if (e.key === "Enter") {
-    if (e.shiftKey) {
-      return;
-    }
-
-    e.preventDefault();
-    e.stopPropagation();
-
-    const message = input.value.trim();
-
-    sendMessage(message,input);
-  }
+  const message = messageBox.value.trim();
+  console.log(message);
+  sendMessage(message, messageBox);
 }
 
-function sendMessage(message,input) {
+function sendMessage(message, input) {
   if (message !== "") {
     const encodedMessage = encodeURIComponent(message);
     const whatsappUrl = `https://wa.me/5521965494017?text=${encodedMessage}`;
-
     window.open(whatsappUrl, "_blank");
 
     input.value = "";
--- src/js/controller.js
+++ src/js/controller.js
@@ -2,24 +2,20 @@ import { showProjects, updateProjectDetails } from "./ui.js";
 import { getProjects } from "./projects.js";
 import { contactMessage } from "./contact.js";
 
-const projectsThree = Array.from(document.querySelectorAll(".project"));
-const messageBox = document.getElementById("contact-message");
-
-messageBox.addEventListener("keydown", (e) => {
-  contactMessage(e, messageBox);
-});
-
-projectsThree.forEach((branch) => {
-  branch.addEventListener("click", (e) => {
-    let projectFind = getProjects().find(
-      (p) => p.fullName === branch.textContent,
-    );
-    if (projectFind) {
-      updateProjectDetails(projectFind);
-    }
-  });
-});
-
 export function initPortfolio() {
+  const projectsThree = Array.from(document.querySelectorAll(".project"));
+  const btnSendMessage = document.getElementById("btn-send-message");
+  btnSendMessage.addEventListener("click", contactMessage);
+  projectsThree.forEach((branch) => {
+    branch.addEventListener("click", (e) => {
+      let projectFind = getProjects().find(
+        (p) => p.fullName === branch.textContent,
+      );
+      if (projectFind) {
+        updateProjectDetails(projectFind);
+      }
+    });
+  });
+
   showProjects(projectsThree, getProjects());
 }
